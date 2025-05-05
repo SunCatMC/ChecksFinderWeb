8ca0a6ff425185486b451091159b904c88c028c8
SunCat <suncat.game@ya.ru>
2025-05-05 11:28:58 +0300

    implement long tap detection

--- ChecksFinder.service.worker.js
+++ ChecksFinder.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746430419|7724953691';
+const CACHE_VERSION = '1746433701|11006706181';
 /** @type {string} */
 const CACHE_PREFIX = 'ChecksFinder-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
