9dcd4c5027397e79c3699814cc2b250bcf21fa1b
SunCat <suncat.game@ya.ru>
2025-05-05 10:34:12 +0300

    test without emulating mouse

--- ChecksFinder.service.worker.js
+++ ChecksFinder.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746427307|4612899113';
+const CACHE_VERSION = '1746430419|7724953691';
 /** @type {string} */
 const CACHE_PREFIX = 'ChecksFinder-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
