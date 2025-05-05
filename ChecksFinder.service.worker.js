8d4296351be7f20c1a3e7cd8574f3b8c3a4a8105
SunCat <suncat.game@ya.ru>
2025-05-05 08:28:00 +0300

    update with latest

--- ChecksFinder.service.worker.js
+++ ChecksFinder.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746287948|13339169965';
+const CACHE_VERSION = '1746422776|81827475';
 /** @type {string} */
 const CACHE_PREFIX = 'ChecksFinder-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
